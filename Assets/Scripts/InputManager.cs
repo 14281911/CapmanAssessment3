c80a10cbf14b86dde5becf6a6dfac2f808939ed9
14281911 <daniel.kagan@student.uts.edu.au>
2021-09-27 16:49:50 +1000

    Capman tweening changed to clockwise

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -16,10 +16,10 @@ public class InputManager : MonoBehaviour
     {
         tweener = GetComponent<Tweener>();
 
-        capmanRight = new Vector3(-12.0f, 9.0f, 0.0f);
-        capmanUp = new Vector3(-7.0f, 9.0f, 0.0f);
-        capmanLeft = new Vector3(-7.0f, 13.0f, 0.0f);
-        capmanDown = new Vector3(-12.0f, 13.0f, 0.0f);
+        capmanRight = new Vector3(-12.0f, 13.0f, 0.0f);
+        capmanUp = new Vector3(-12.0f, 9.0f, 0.0f);
+        capmanLeft = new Vector3(-7.0f, 9.0f, 0.0f);
+        capmanDown = new Vector3(-7.0f, 13.0f, 0.0f);
     }
 
     
@@ -27,22 +27,22 @@ public class InputManager : MonoBehaviour
     {
         if (item.transform.position == capmanRight)
         {
-            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-7.0f, 9.0f, 0.0f), 2.0f);
+            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-7.0f, 13.0f, 0.0f), 2.0f);
 		}
 
         if (item.transform.position == capmanUp)
         {
-            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-7.0f, 13.0f, 0.0f), 1.5f);
+            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-12.0f, 13.0f, 0.0f), 1.5f);
 		}
 
         if (item.transform.position == capmanLeft)
         {
-            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-12.0f, 13.0f, 0.0f), 2.0f);
+            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-12.0f, 9.0f, 0.0f), 2.0f);
 		}
 
         if (item.transform.position == capmanDown)
         {
-            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-12.0f, 9.0f, 0.0f), 1.5f);
+            tweener.AddTween(item.transform, item.transform.position, new Vector3 (-7.0f, 9.0f, 0.0f), 1.5f);
 		}
     }
 }
